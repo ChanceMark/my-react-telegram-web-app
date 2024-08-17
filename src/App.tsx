d55798c157b68a8bafea936585fc2c1384ac1ecc
Chance Mark <chance8476@outlook.com>
2024-08-18 01:50:58 +0900

    next

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import WebApp from '@twa-dev/sdk'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,13 +23,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/* Here we add our button with alert callback */}
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+            Show Alert
+        </button>
+      </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
